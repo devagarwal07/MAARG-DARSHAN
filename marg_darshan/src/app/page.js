a80ece6cd7a5a8e4c7f487acb975b5bbd290a265
AvikRay1001 <avikray1001@gmail.com>
2024-09-03 22:32:25 +0530

    Day2

--- marg_darshan/src/app/page.js
+++ marg_darshan/src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HomePage from "@/components/homepage/HomePage";
 import About from "@/components/about/About";
 import Services from "@/components/ourservices/services";
+import Testimonials from "@/components/testimonials/Testimonials";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -16,6 +17,9 @@ export default function Home() {
       </section>
       <section>
         <Services/>
+      </section>
+      <section>
+        <Testimonials/>
       </section>
     </>
   );
